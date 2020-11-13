afdf986fb3f3d6bdccf2fbc513512f38b0999115
Kamlesh Kirodewal <kkirodewal@gmail.com>
2020-11-13 15:03:40 +0530

    Create aria.py

--- aria.py
+++ aria.py
@@ -10,22 +10,22 @@ aria2 = aria2p.API(
     )
 )
 
-def addDownload(link):
-    link = link.replace('/mirror','')
-    link = link.strip()
-    print(link)
-    download = aria2.add_magnet(link)
-    while download.is_active:
-        print("downloading")
-    if(download.is_complete):
-        print("Download complete")
+# initialization, these are the default values
+aria2 = aria2p.API(
+    aria2p.Client(
+        host="http://localhost",
+        port=6800,
+        secret=""
+    )
+)
 
 # list downloads
-   downloads = aria2.get_downloads(list)
+downloads = aria2.get_downloads()
 
-def main():
-    for download in downloads:
-        print(download.name, download.download_speed)
+for download in downloads:
+    print(download.name, download.download_speed)
 
-if __name__ == "__main__":
-    main()
+# add downloads
+magnet_uri = "magnet:?xt=urn:..."
+
+download = aria2.add_magnet(magnet_uri)
